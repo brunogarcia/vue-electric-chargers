c7e676d34647d6117b73ec4d0c6ee5bc70118c7c
Bruno Garcia <bruno@garciaechegaray.com>
2022-02-05 18:32:08 +0100

    [feat] Create the service chargers

---
+++ frontend/src/http/index.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from "axios";
+import { Charger } from "@/store/store.types";
+
+/**
+ * Fetch the chargers list
+ *
+ * @returns {Charger[]} Charger list
+ */
+async function fetchChargers(): Promise<AxiosResponse<Charger[]>> {
+  try {
+    const response = await axios.get("/api/chargers");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export default {
+  fetchChargers,
+};
